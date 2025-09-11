0023b9eab30b1604e8ee2450c7bf8114e85719ea
K4milJ <jamroszczyk.kamil@gmail.com>
2025-09-11 21:33:24 +0200

    Added main file, and header file.

---
+++ philo/include/philo.h
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <stdbool.h>
+#include <pthread.h>
+
+typedef struct	s_fork
+{
+	pthread_mutex_t	fork;
+	int				fork_id;
+}	t_fork;
+
+typedef struct	s_philo
+{
+	int				philo_id;
+	long			meal_count;
+	bool			is_full;
+	t_fork			*first_fork;
+	t_fork			*second_fork;
+}	t_philo;
+
+typedef struct	s_table
+{
+	long			philo_num;
+	long			time_to_die;
+	long			time_to_eat;
+	long			time_to_sleep;
+	long			meal_limit;
+
+	t_philo			*philos;
+	t_fork			*forks;
+}	t_table;
---
+++ philo/src/philo.c
@@ -0,0 +1,19 @@
+#include "../include/philo.h"
+
+int main(int argc, char **argv)
+{
+	//printf("argc = %d\n", argc);
+
+	if (argc == 5 || argc == 6)
+	{
+		// input_check();
+		// data_init();
+		// clean();
+	}
+	else
+	{
+		printf("Wrong input!");
+	}
+	
+	return (0);
+}
